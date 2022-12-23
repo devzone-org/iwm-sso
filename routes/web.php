134b0badba75c93a88711094ce5c34d282d32ae0
Ali <ali2020ahtisham@outlook.com>
2022-12-23 18:52:45 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing-page');
 });
 
 Route::get('employees/assign-portal/{id}', [\App\Http\Controllers\EmployeeController::class, 'index']);
